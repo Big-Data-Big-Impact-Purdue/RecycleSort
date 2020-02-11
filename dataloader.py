60de133173ccd7e1e9ee599194776348fdb86b07
Aditya Chakraborty <chakra17@purdue.edu>
2020-02-11 02:14:20 -0500

    Added train/val/test split after download

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,68 @@
 import os
+import shutil
 import matplotlib.pyplot as plt
 from keras.preprocessing import image
 import numpy as np
+import pandas as pd
+import random
+import split_folders
 
+
+def create_directories(path):
+
+    try:
+        os.mkdir("input")
+    except:
+        pass
+
+    try:
+        os.mkdir("output")
+    except:
+        pass
+
+    path1 = path + "/input/PlasticBottles"
+    try:
+        os.mkdir(path1)
+    except:
+        pass
+
+    path2 = path + "/input/MetalCans"
+    try:
+        os.mkdir(path2)
+    except:
+        pass
+
+
+
+def split_data(data_dir, csv_file):
+    ImageLabels = pd.read_csv(data_dir + '/' + csv_file, header=None)
+    ImageDir = {}
+    path = data_dir + '/'
+    output_dir = path + "output/"
+    dictImageLabels = {}
+    filenames = []
+    create_directories(path)
+
+
+    for row in ImageLabels.index:
+        dictImageLabels[ImageLabels[0][row]] = ImageLabels[1][row]
+
+    for f in filenames:
+        if dictImageLabels[f] == 'PlasticBottles':
+            shutil.copyfile(path + 'data/' + f, 'input/PlasticBottles/'+f)
+        elif dictImageLabels[f] == 'MetalCans':
+            shutil.copyfile(path + 'data/' + f, 'input/MetalCans/'+f)
+
+
+    split_folders.ratio(path + 'input/', output=output_dir, seed = 10, ratio=(.8, .1, .1))
+
+def download_from_S3():
+    pass
+
+
+def DataLoader(path):
+
+    pass
 
 
 class TestDataLoader():
@@ -44,3 +104,7 @@ class TestDataLoader():
             images[f] = image_tensor
 
         return images
+
+print (os.getcwd())
+#os.chdir("../")
+split_data(os.getcwd(), "image_labels.csv")
